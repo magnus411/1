3ff228e1d01acadece56c053fb6b351df5a80db2
Magnus411 <magnusgjerstad00@gmail.com>
2022-09-20 10:11:20 +0200

    ye

--- Oppgave2/Oppgave2/main.py
+++ Oppgave2/Oppgave2/main.py
@@ -12,6 +12,79 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Click "Open user guide" on the EV3 extension tab for more information.
 
 
+
+#if sensor is trykked do def()
+#Utrasonis for å ungå trær 
+import time
+#Si exersice 2 ganger
+motor_left = Motor(Port.A)
+motor_right = Motor(Port.B)
+
+robot = DriveBase(motor_left, motor_right, wheel_diameter=49.6, axle_track=105)
+robot.settings(1000)
+#ts = TouchSensor()
+
+us = UltrasonicSensor(Port.S3)
+ts = TouchSensor(Port.S4)
+
+ts2 = TouchSensor(Port.S1)
+ts3 = TouchSensor(Port.S2)
+
+
+drive = False
+
+while True:
+    if ts.pressed() and drive == False:
+        drive = True
+
+
+    while drive == True:
+
+        if ts.pressed() and drive == True:
+            drive = False
+
+
+        if us.distance() < 300:
+            robot.drive(200, 0)  
+        else:
+            robot.drive(1000, 0)
+
+        if ts2.pressed() or ts3.pressed():
+            robot.straight(-300)
+            time.sleep(1)
+            robot.turn(35)
+            robot.drive(1000,0)
+
+
+    
+    robot.drive(0,0)
+
+
+    
+
+
+        #robot.straight()
+    
+
+
+
+
+    
+
+    
+
+    #robot.turn(90)
+        
+
+
+    
+    #time.sleep(1)
+    #ev3.speaker.say("Have a nice day")
+
+
+
+
+# 
 # Create your objects here.
 ev3 = EV3Brick()
 
