e61d867d55d1f646279b5b70dae59b5f29cbeff8
trygjor <trygjor@stud.ntnu.no>
2022-10-04 09:33:58 +0200

    testupdate

--- Oppgave4/main.py
+++ Oppgave4/main.py
@@ -25,23 +25,37 @@ ev3 = EV3Brick()
 ev3.speaker.beep()
 
 colorConst = 30
-speedConst = 300
-turnConst = 75
+speedConst = 2
+turnConst = 10
 
+#Sens max100 min 6
+#Sens max89  min 6
 
+#Diff sens1 - sens2 = 90
+#diff sens1 - sens2 = -79
+#diff sens1 - sens2 = 10
 
 
 def Drive():
-    if (colorSenseR.reflection() < colorConst):
-        robot.drive(speedConst, turnConst)
-    elif(colorSenseL.reflection() < colorConst):
-        robot.drive(speedConst, -turnConst)
-    else:
-        robot.drive(speedConst, 0)
+    turnRate =  (colorSenseL.reflection() - colorSenseR.reflection() + 12)
+    driveRate = (colorSenseL.reflection() + colorSenseR.reflection() - 90) * speedConst
+
+    #print(turnRate)
+    robot.drive(driveRate, turnRate)
+
+
+#
+ #   if (colorSenseR.reflection() < colorConst):
+  #      robot.drive(speedConst, turnConst)
+#    elif(colorSenseL.reflection() < colorConst):
+#        robot.drive(speedConst, -turnConst)
+#    else:
+#       robot.drive(speedConst, 0)
 
 
 
 while(True):
+    
     Drive()
 
 
