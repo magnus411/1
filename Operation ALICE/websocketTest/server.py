96e699c300b40c791ecc1b062e8f534e58e9c498
Magnus411 <magnusgjerstad00@gmail.com>
2022-11-10 12:17:56 +0100

    test

--- Operation ALICE/websocketTest/server.py
+++ Operation ALICE/websocketTest/server.py
@@ -12,7 +12,7 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 
 # takes the first argument from command prompt as IP address
-IP_address = str("10.22.7.63")
+IP_address = str("10.22.6.114")
 
 # takes second argument from command prompt as port number
 Port = int("1024")
